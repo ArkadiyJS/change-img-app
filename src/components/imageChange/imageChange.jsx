2f0fe4932e95717226d448ca2fc3c29119c4968f
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2023-11-01 20:58:16 +0500

    info usersam

--- src/components/imageChange/imageChange.jsx
+++ src/components/imageChange/imageChange.jsx
@@ -36,6 +36,7 @@ const ImageEditor = () => {
       <input className='button' type="file" onChange={handleFileChange} multiple />
       <input className='button1' type="number" min="0" placeholder="ширина" value={width} onChange={(e) => setWidth(e.target.value)} />
       <input className='button1' type="number" min="0" placeholder="высота" value={height} onChange={(e) => setHeight(e.target.value)} />
+      <h1>Изменять и Скачивать нужно по 10 фото за раз </h1>
       <button className='button' onClick={handleResize}>Изменить и скачать</button>
       {files.map((file) => (
         <img key={file} src={file} alt="Uploaded" />
