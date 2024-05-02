6aa94161a3c575b9cfd465daf4718fdc13778241
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2024-05-02 21:26:32 +0500

    return

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import './App.css';
-import CameraApp from './components/camera';
 
-// import ImageEditor from './components/imageChange/imageChange';
+
+import ImageEditor from './components/imageChange/imageChange';
 
 function App() {
   return (
     <div className="container">
 
-      {/* <ImageEditor /> */}
+      <ImageEditor />
 
-      <CameraApp />
+
 
     </div>
   );
